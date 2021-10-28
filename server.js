edecab00c97240abfc7b09a363949ed4a346f6e7
unknown <>
2021-10-29 01:33:31 +0300

    initial

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+'/index.html');
+})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is connected at port 3000");
+})
