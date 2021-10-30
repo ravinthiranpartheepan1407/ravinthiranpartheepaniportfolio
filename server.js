e7f880f0bf86449cf48db16ee328a2841bd7be78
unknown <>
2021-10-31 01:33:04 +0300

    secondcommit

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
   res.sendFile(__dirname+'/index.html');
+})
+
+app.get("/vrapp", function(req, res){
+  res.render('vrapp');
 })
 
 app.listen(process.env.PORT || 3000, function(){
