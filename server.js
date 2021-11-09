30f23b94fc546cf63a4fbaaea473affe747b63a1
Suren Partheepan <ravinthiranpartheepan1407@gmail.com>
2021-11-09 08:27:31 -0800

    twentyfour

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const nodemailer = require('nodemailer');
 const app = express();
+var crypto = require("crypto");
+ids = crypto.randomBytes(16).toString('hex');;
+idss = crypto.randomBytes(20).toString('hex');;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -39,6 +42,41 @@ app.get("/login", function(req, res){
   res.render('login');
 })
 
+
+app.post('/contact', function(req, res) {
+  const link = "https://ravinthiranpartheepan.com/ethapp?id=" +ids
+  const vrapp = "https://ravinthiranpartheepan.com/vrapp?id=" +idss
+  const name = req.body.name;
+  const email = req.body.emails;
+  const subject = req.body.subject;
+  const messsage = req.body.message;
+  let transporters = nodemailer.createTransport({
+    service: "Gmail",
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'ravinthiran@ofoundation.nl',
+      pass: 'Nodedoodle@doodle1407'
+    }
+  });
+  let mailOptions = {
+    from: '"Ravinthiran Partheepan" <ravinthiran1407@gmail.com>',
+    to: email,
+    subject: "Thank you for contacting me - Ravinthiran Partheepan",
+    html: "<p>Dear " + name + " </p><p>Thank you again for contacting me! I will respond to you message as soon as possible. In the mean time checkout my ethereum shop:"+ link +" </p><p> Oh! I have a gift for you. Here's my VR App session for you "+ vrapp +"</p>"
+
+
+  };
+
+  transporters.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message  sent: ', info.messageId, info.response);
+    res.redirect('/home');
+  });
+  console.log(email)
+});
 
 
 app.get("/ethapp", function(req, res){
